5b3686da064677e76b9302ee93dba731e3bf4501
Spencer Herzberg <spencer.herzberg@gmail.com>
2014-09-16 21:49:19 -0600

    dynamic env PORT for marathon config

--- app.py
+++ app.py
@@ -34,5 +34,9 @@ def health():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=os.environ.get('DEBUG', 'false').lower() == 'true')
+    app.run(
+        host='0.0.0.0',
+        port=int(os.environ.get('PORT', 5000)),
+        debug=os.environ.get('DEBUG', 'false').lower() == 'true'
+    )
 
